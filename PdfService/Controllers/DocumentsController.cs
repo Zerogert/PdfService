7991f4b2fbaabb7997861e808cbc5e2eaf152888
Zerogert <zlodey.deadalive@gmail.com>
2022-04-08 12:16:14 +0700

    Add project files.

---
+++ PdfService/Controllers/DocumentsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PuppeteerSharp;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace PdfService.Controllers {
+	[ApiController]
+	[Route("[controller]")]
+	public class DocumentsController: ControllerBase {
+
+		private readonly ILogger<DocumentsController> _logger;
+
+		public DocumentsController(ILogger<DocumentsController> logger) {
+			_logger = logger;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Post(string url, bool landscape) {
+			return File(await GeneratePdfAsync(url, landscape), "application/pdf");
+		}
+
+		public static async Task<Stream> GeneratePdfAsync(string url, bool landscape) {
+			var options = new LaunchOptions { Headless = true, ExecutablePath = @"C:\Users\Zeroget\Downloads\chrome-win\chrome-win\chrome.exe", Args = new string[] { "--no-sandbox" } };
+			using (var browser = await Puppeteer.LaunchAsync(options))
+			using (var page = await browser.NewPageAsync()) {
+				await page.GoToAsync(url);
+				return await page.PdfStreamAsync(new PdfOptions() { Format = PuppeteerSharp.Media.PaperFormat.A4, Landscape = landscape, OmitBackground = true, PrintBackground = true });
+			}
+		}
+	}
+}
