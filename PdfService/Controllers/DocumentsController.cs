87c7080d8952fd953a9cf2aa17ab95871c941f73
Zerogert <zlodey.deadalive@gmail.com>
2022-04-11 14:07:13 +0700

    add old controller

--- PdfService/Controllers/DocumentsController.cs
+++ PdfService/Controllers/DocumentsController.cs
@@ -1,21 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using PuppeteerSharp;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace PdfService.Controllers {
 	[ApiController]
-	[Route("v2/[controller]")]
+	[Route("[controller]")]
 	public class DocumentsController: ControllerBase {
-		private readonly LaunchOptions _browserOptions = new LaunchOptions { Headless = true, ExecutablePath = @"C:\Users\Zeroget\Downloads\chrome-win\chrome-win\chrome.exe", Args = new string[] { "--no-sandbox" } };
-		//private readonly LaunchOptions _browserOptions = new LaunchOptions { Headless = true, ExecutablePath = @"/usr/bin/chromium-browser", Args = new string[] { "--no-sandbox" } };
-		private static Browser s_browser = null;
 
 		private readonly ILogger<DocumentsController> _logger;
 
@@ -23,53 +15,18 @@ namespace PdfService.Controllers {
 			_logger = logger;
 		}
 
-		[HttpPost("byUrl")]
-		public async Task<IActionResult> Get(List<string> url, bool landscape) {
-			Stopwatch stopwatch = Stopwatch.StartNew();
-			stopwatch.Start();
-			var files = await GeneratePdfAsync(url, landscape);
-			return Ok(new { Paths = files.Select(x => "/pdf/" + x), Elapsed = stopwatch.ElapsedMilliseconds });
+		[HttpGet]
+		public async Task<IActionResult> Post(string url, bool landscape) {
+			return File(await GeneratePdfAsync(url, landscape), "application/pdf");
 		}
 
-		[HttpPost("byFile")]
-		public async Task<IActionResult> Post(IFormFileCollection fileStream, bool landscape) {
-			Stopwatch stopwatch = Stopwatch.StartNew();
-			stopwatch.Start();
-			var files = await GeneratePdfAsync(fileStream.ToList(), landscape);
-			return Ok(new { Paths = files.Select(x => "/pdf/" + x), Elapsed = stopwatch.ElapsedMilliseconds });
-		}
-
-		[NonAction]
-		public async Task<List<string>> GeneratePdfAsync(List<IFormFile> streams, bool landscape) {
-			s_browser ??= await Puppeteer.LaunchAsync(_browserOptions);
-			var tasks = streams.Select(async x => {
-				using (StreamReader stringReader = new StreamReader(x.OpenReadStream()))
-				using (var page = await s_browser.NewPageAsync()) {
-					await page.SetContentAsync(await stringReader.ReadToEndAsync());
-					var fileName = $"{Guid.NewGuid()}.pdf";
-					await page.PdfAsync($"{Directory.GetCurrentDirectory()}/files/{fileName}", new PdfOptions() { Format = PuppeteerSharp.Media.PaperFormat.A4, Landscape = landscape, OmitBackground = true, PrintBackground = true });
-					return fileName;
-				}
-			});
-
-			var files = await Task.WhenAll(tasks);
-			return files.ToList();
-		}
-
-		[NonAction]
-		public async Task<List<string>> GeneratePdfAsync(List<string> url, bool landscape) {
-			s_browser ??= await Puppeteer.LaunchAsync(_browserOptions);
-			var tasks = url.Select(async x => {
-				using (var page = await s_browser.NewPageAsync()) {
-					await page.GoToAsync(x);
-					var fileName = $"{Guid.NewGuid()}.pdf";
-					await page.PdfAsync($"{Directory.GetCurrentDirectory()}/files/{fileName}", new PdfOptions() { Format = PuppeteerSharp.Media.PaperFormat.A4, Landscape = landscape, OmitBackground = true, PrintBackground = true });
-					return fileName;
-				}
-			});
-
-			var files = await Task.WhenAll(tasks);
-			return files.ToList();
+		public static async Task<Stream> GeneratePdfAsync(string url, bool landscape) {
+			var options = new LaunchOptions { Headless = true, ExecutablePath = @"/usr/bin/chromium-browser", Args = new string[] { "--no-sandbox" } };
+			using (var browser = await Puppeteer.LaunchAsync(options))
+			using (var page = await browser.NewPageAsync()) {
+				await page.GoToAsync(url);
+				return await page.PdfStreamAsync(new PdfOptions() { Format = PuppeteerSharp.Media.PaperFormat.A4, Landscape = landscape, OmitBackground = true, PrintBackground = true });
+			}
 		}
 	}
 }
---
+++ PdfService/Controllers/DocumentsV2Controller.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PuppeteerSharp;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PdfService.Controllers {
+	[ApiController]
+	[Route("[controller]")]
+	public class DocumentsV2Controller: ControllerBase {
+		private readonly LaunchOptions _browserOptions = new LaunchOptions { Headless = true, ExecutablePath = @"C:\Users\Zeroget\Downloads\chrome-win\chrome-win\chrome.exe", Args = new string[] { "--no-sandbox" } };
+		//private readonly LaunchOptions _browserOptions = new LaunchOptions { Headless = true, ExecutablePath = @"/usr/bin/chromium-browser", Args = new string[] { "--no-sandbox" } };
+		private static Browser s_browser = null;
+
+		private readonly ILogger<DocumentsV2Controller> _logger;
+
+		public DocumentsV2Controller(ILogger<DocumentsV2Controller> logger) {
+			_logger = logger;
+		}
+
+		[HttpPost("byUrl")]
+		public async Task<IActionResult> Get(List<string> url, bool landscape) {
+			Stopwatch stopwatch = Stopwatch.StartNew();
+			stopwatch.Start();
+			var files = await GeneratePdfAsync(url, landscape);
+			return Ok(new { Paths = files.Select(x => "/pdf/" + x), Elapsed = stopwatch.ElapsedMilliseconds });
+		}
+
+		[HttpPost("byFile")]
+		public async Task<IActionResult> Post(IFormFileCollection fileStream, bool landscape) {
+			Stopwatch stopwatch = Stopwatch.StartNew();
+			stopwatch.Start();
+			var files = await GeneratePdfAsync(fileStream.ToList(), landscape);
+			return Ok(new { Paths = files.Select(x => "/pdf/" + x), Elapsed = stopwatch.ElapsedMilliseconds });
+		}
+
+		[NonAction]
+		public async Task<List<string>> GeneratePdfAsync(List<IFormFile> streams, bool landscape) {
+			s_browser ??= await Puppeteer.LaunchAsync(_browserOptions);
+			var tasks = streams.Select(async x => {
+				using (StreamReader stringReader = new StreamReader(x.OpenReadStream()))
+				using (var page = await s_browser.NewPageAsync()) {
+					await page.SetContentAsync(await stringReader.ReadToEndAsync());
+					var fileName = $"{Guid.NewGuid()}.pdf";
+					await page.PdfAsync($"{Directory.GetCurrentDirectory()}/files/{fileName}", new PdfOptions() { Format = PuppeteerSharp.Media.PaperFormat.A4, Landscape = landscape, OmitBackground = true, PrintBackground = true });
+					return fileName;
+				}
+			});
+
+			var files = await Task.WhenAll(tasks);
+			return files.ToList();
+		}
+
+		[NonAction]
+		public async Task<List<string>> GeneratePdfAsync(List<string> url, bool landscape) {
+			s_browser ??= await Puppeteer.LaunchAsync(_browserOptions);
+			var tasks = url.Select(async x => {
+				using (var page = await s_browser.NewPageAsync()) {
+					await page.GoToAsync(x);
+					var fileName = $"{Guid.NewGuid()}.pdf";
+					await page.PdfAsync($"{Directory.GetCurrentDirectory()}/files/{fileName}", new PdfOptions() { Format = PuppeteerSharp.Media.PaperFormat.A4, Landscape = landscape, OmitBackground = true, PrintBackground = true });
+					return fileName;
+				}
+			});
+
+			var files = await Task.WhenAll(tasks);
+			return files.ToList();
+		}
+	}
+}
